0b970d8865760637ef3d7ccd22cd125eb71ede2f
Pedro Silva <61997113+Pedro050700@users.noreply.github.com>
2021-08-26 14:16:02 -0300

    Update xmlforxlsx.js

--- xmlforxlsx.js
+++ xmlforxlsx.js
@@ -3,12 +3,19 @@ const fs = require('fs');
 const util = require("util");
 const json2xls = require("json2xls");
 
+                                     
 var xml = require('fs').readFileSync('NFE.xml', 'utf8');
-var options = { compact: true, spaces: 4, indentCdata: false, ignoreComment: true, ignoreCdata: true };
-var result = convert.xml2js(xml, options); // or convert.xml2json(xml, options)
-// console.log(util.inspect(result, false, null, true));
-console.log(util.inspect(result.nfeProc.NFe.infNFe.emit.CNPJ._text, false, null, true));
+var options = { compact: true, spaces: 4, ignoreComment: true, ignoreCdata: true };
+var result = convert.xml2js(xml, options); 
 
-//aqui eu crio uma variavel onde eu passo o meu json para xlsx
-let xls = json2xls(result.nfeProc.NFe.infNFe.emit);
+ var dest = {
+    cnpj: result.nfeProc.NFe.infNFe.dest.CNPJ._text,
+    nome: result.nfeProc.NFe.infNFe.dest.xNome._text,
+ }
+ console.log(dest);
+
+// console.log(util.inspect(result.nfeProc.NFe.infNFe.dest, false, null, true));
+
+// aqui eu crio uma variavel onde eu passo o meu json para xlsx
+ var xls = json2xls(dest);
 fs.writeFileSync("NFE.xlsx", xls, "binary");
